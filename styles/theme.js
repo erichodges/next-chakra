9665f5a2c5df9d527c8bb13e8c8dfd6e5965ec76
Eric Hodges <ehodges2004@gmail.com>
2021-08-05 17:01:33 -0700

    Added Chakra UI and initial setup

---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const customTheme = extendTheme({
+  colors: {
+    test: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
+    }
+  }
+})
+
+
+
